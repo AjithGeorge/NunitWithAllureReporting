76ccbe28c7b77c80d02fcecda24a8071dfb80617
Ajith George <Ajith.George@neudesic.com>
2019-10-01 21:10:38 +0530

    Updating missing items new version

--- NunitWithAllureReporting_Version1/Sample_AllureReportTypes.cs
+++ NunitWithAllureReporting_Version1/Sample_AllureReportTypes.cs
@@ -7,54 +7,56 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 
-namespace WhiteCase.CompApp.UiTests.Scenarios
+namespace AllureSample
 {
-    [TestFixture]
-    [AllureNUnit]
-    [AllureSubSuite("AllureReportTypeSamples")]
     class Sample_AllureReportTypes
     {
-        [SetUp]
-        public void SetUp()
+        [TestFixture]
+        [AllureNUnit]
+        [AllureSubSuite("AllureReportTypeSamples")]
+        class TestClass
         {
-            Console.WriteLine("Hello, this is TestClass1 setUp");
-        }
-
-        [Test(Description = "Sample")]
-        [AllureTag("TC-1")]
-        [AllureIssue("ISSUE-1")]
-        [AllureTms("TMS-12")]
-        [AllureOwner("unickq")]
-        [AllureSuite("PassedSuite")]
-        [AllureSubSuite("NoAssert")]
-        [AllureSubSuite("Simple")]
-        public void SimpleTestPassed()
-        {
-            Console.WriteLine(DateTime.Now);
-        }
-
-        [Test]
-        [Ignore("I'm just an ignored test")]
-        public void SimpleTestIgnored1()
-        {
-        }
-
-        [Test(Author = "unickq")]
-        [Description("OLOLO")]
-        [AllureSuite("FailedSuite")]
-        [AllureSubSuite("DoesNotThrow")]
-        [AllureLink("Google", "https://google.com")]
-        public void SimpleTestFailed()
-        {
-            Console.WriteLine("Failed");
-            Assert.DoesNotThrow(() =>
+            [SetUp]
+            public void SetUp()
             {
-                throw new Exception("I'm an exception");
-            });
-        }
+                Console.WriteLine("Hello, this is TestClass1 setUp");
+            }
 
-        private readonly string[] _adeleSong =
-        {
+            [Test(Description = "Sample")]
+            [AllureTag("TC-1")]
+            [AllureIssue("ISSUE-1")]
+            [AllureTms("TMS-12")]
+            [AllureOwner("unickq")]
+            [AllureSuite("PassedSuite")]
+            [AllureSubSuite("NoAssert")]
+            [AllureSubSuite("Simple")]
+            public void SimpleTestPassed()
+            {
+                Console.WriteLine(DateTime.Now);
+            }
+
+            [Test]
+            [Ignore("I'm just an ignored test")]
+            public void SimpleTestIgnored1()
+            {
+            }
+
+            [Test(Author = "unickq")]
+            [Description("OLOLO")]
+            [AllureSuite("FailedSuite")]
+            [AllureSubSuite("DoesNotThrow")]
+            [AllureLink("Google", "https://google.com")]
+            public void SimpleTestFailed()
+            {
+                Console.WriteLine("Failed");
+                Assert.DoesNotThrow(() =>
+                {
+                    throw new Exception("I'm an exception");
+                });
+            }
+
+            private readonly string[] _adeleSong =
+            {
         "Hello, it's me",
         "I was wondering if after all these years you'd like to meet",
         "To go over everything",
@@ -62,20 +64,20 @@ namespace WhiteCase.CompApp.UiTests.Scenarios
         "But I ain't done much healing"
     };
 
-        [Test]
-        [AllureEpic("Song")]
-        [AllureSuite("Adele")]
-        [AllureSubSuite("Hello")]
-        public void TestWithSteps()
-        {
-            //Allure Steps Example
-            foreach (var str in _adeleSong)
-                AllureLifecycle.Instance.WrapInStep(() =>
-                {
-                    Thread.Sleep(100 * new Random().Next(1, 10));
-                    Console.WriteLine(str + Environment.NewLine);
-                }, str);
+            [Test]
+            [AllureEpic("Song")]
+            [AllureSuite("Adele")]
+            [AllureSubSuite("Hello")]
+            public void TestWithSteps()
+            {
+                //Allure Steps Example
+                foreach (var str in _adeleSong)
+                    AllureLifecycle.Instance.WrapInStep(() =>
+                    {
+                        Thread.Sleep(100 * new Random().Next(1, 10));
+                        Console.WriteLine(str + Environment.NewLine);
+                    }, str);
+            }
         }
     }
 }
-
